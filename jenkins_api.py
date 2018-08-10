6303d0c20ec85a10f15f57791f08a7fe30719500
Brittain Hard <brittainhard@gmail.com>
2018-08-10 12:26:55 -0500

    Using the jenkins API to get info on jobs.

--- jenkins_api.py
+++ jenkins_api.py
@@ -2,5 +2,14 @@ import requests
 
 from secrets import user, password
 
-r = requests.get("http://18.191.94.64/job/goai-docker-container-builder/api/json?tree=builds", auth=(user, password))
-print(r.text)
+# r = requests.get("http://18.191.94.64/job/goai-docker-container-builder/api/json?tree=builds", auth=(user, password))
+
+import jenkinsapi
+from jenkinsapi.jenkins import Jenkins
+
+j = Jenkins("http://18.191.94.64/")
+jobs = []
+for item in j.items():
+    jobs.append(item[1])
+
+last_good_jobs = [job.get_last_good_build().get_timestamp() for job in jobs]
