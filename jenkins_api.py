01e116f6d5d07ef882c8104db009580d3a198c6c
Brittain Hard <brittainhard@gmail.com>
2018-08-15 18:31:52 -0500

    Don't check if itself is running.

--- jenkins_api.py
+++ jenkins_api.py
@@ -2,10 +2,17 @@ import jenkinsapi
 from jenkinsapi.jenkins import Jenkins
 
 
+NON_GPU_JOBS = ["goai-docker-container-builder", "gpu-instance-manager"]
+
+
 def get_jobs():
     jenk = Jenkins("http://18.191.94.64/")
     jobs = []
     for item in jenk.items():
+        if str(item[1]) in [str(job) for job in jobs]:
+            continue
+        elif str(item[1]) in NON_GPU_JOBS:
+            continue
         jobs.append(item[1])
     return jobs
 
