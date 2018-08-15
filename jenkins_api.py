2e2433c0b807d2af7079eef078c1db6c5db26f98
Brittain Hard <brittainhard@gmail.com>
2018-08-15 18:11:48 -0500

    Script that checks to see if any jobs are running .

--- jenkins_api.py
+++ jenkins_api.py
@@ -1,14 +1,19 @@
-import datetime
-
 import jenkinsapi
 from jenkinsapi.jenkins import Jenkins
 
-j = Jenkins("http://18.191.94.64/")
-jobs = []
-for item in j.items():
-    jobs.append(item[1])
 
-last = [job.get_last_good_build().get_timestamp() for job in jobs]
-now = datetime.datetime.now(tz=datetime.timezone.utc)
-now = now.replace(second=0, microsecond=0)
-print(now)
+def get_jobs():
+    jenk = Jenkins("http://18.191.94.64/")
+    jobs = []
+    for item in jenk.items():
+        jobs.append(item[1])
+    return jobs
+
+
+def jobs_running(jobs):
+    running = [job.is_running() for job in jobs]
+    return any(running)
+
+
+if __name__ == "__main__":
+    print(jobs_running(get_jobs()))
