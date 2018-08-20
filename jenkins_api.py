f43d86a3b37426a625ecaaac7bd63348df230b50
Brittain Hard <brittainhard@gmail.com>
2018-08-20 12:40:02 -0500

    Start connecting to AWS.

--- jenkins_api.py
+++ jenkins_api.py
@@ -1,20 +1,22 @@
 import os, json
 import jenkinsapi
 import requests
-from jenkinsapi.jenkins import Jenkins
+import boto3
 
 
 NON_GPU_JOBS = ["goai-docker-container-builder", "gpu-instance-manager"]
 JENKINS_URL = os.environ.get("JENKINS_URL", "")
 AWS_CREDENTIALS_URL = os.environ.get("AWS_CREDENTIALS_URL", "")
+AWS_KEY = ""
+AWS_KEY_ID = ""
 
 
 def get_jenkins_client(username, password):
-    return Jenkins(JENKINS_URL, username, password)
+    return jenkinsapi.Jenkins(JENKINS_URL, username, password)
 
 
 def get_jobs():
-    jenk = Jenkins(JENKINS_URL)
+    jenk = jenkinsapi.Jenkins(JENKINS_URL)
     jobs = []
     for item in jenk.items():
         if str(item[1]) in [str(job) for job in jobs]:
@@ -31,6 +33,14 @@ def jobs_running(jobs):
 
 
 if __name__ == "__main__":
-    print(jobs_running(get_jobs()))
     r = requests.get(AWS_CREDENTIALS_URL)
-    print(json.loads(r.text).keys())
+    creds = json.loads(r.text)
+    AWS_KEY_ID = creds["AccessKeyId"]
+    AWS_KEY = creds["SecretAccessKey"]
+    ec2 = boto3.client(
+        'ec2',
+        aws_access_key_id=AWS_KEY_ID,
+        aws_secret_access_key=AWS_KEY,
+        region_name="us-east-2"
+    )
+    print(ec2)
