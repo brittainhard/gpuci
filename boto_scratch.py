80c2c81468437aac1c9b80b9fc4035e47edb5bc6
Brittain Hard <brittainhard@gmail.com>
2018-08-23 11:29:11 -0500

    Experimenting with time differences.

---
+++ boto_scratch.py
@@ -0,0 +1,39 @@
+import boto3
+import datetime
+import dateutil
+from secrets import *
+
+
+session = boto3.Session(
+    aws_access_key_id=key_id,
+    aws_secret_access_key=key,
+    region_name=region
+)
+
+
+def running(instance):
+    return instance.state["Code"] == 16
+
+
+def created_on(instance):
+    return instance.launch_time
+
+
+def time_difference(instance):
+    td = created_on(instance)
+    tm = datetime.datetime.now(tz=dateutil.tz.tz.tzutc()) - td
+    hours, remainder = divmod(tm.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return datetime.time(minute=minutes, second=seconds)
+
+
+def close_to_next_hour(instance):
+    time = created_on(instance)
+    potato = datetime.datetime.now(tz=dateutil.tz.tz.tzutc()) - time
+    return potato
+
+
+ec2 = session.resource("ec2")
+instance = list(ec2.instances.iterator())[0]
+diff = time_difference(instance)
+print(diff)
